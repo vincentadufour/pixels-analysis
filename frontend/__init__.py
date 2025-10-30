8b6e0d96f01451adbf0f26a00277d1943703b881
vincentadufour <avdufour@gmail.com>
2025-10-30 13:55:24 -0600

    Worked on frontend

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,8 +1,11 @@
 """Frontend package: Flask application and extensions."""
 
 from flask import Flask
+from werkzeug.utils import secure_filename
 import os
 
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from frontend import routes
